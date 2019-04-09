09e918d0ed7010cdc7521c74777511a8fea4bc52
Aaron Powell <me@aaron-powell.com>
2019-04-09 10:23:27 +1000

    Getting it running with background polling and IoT hubs

--- src/Sunshine.Downloader/Program.fs
+++ src/Sunshine.Downloader/Program.fs
@@ -1,21 +1,26 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open InverterApi
 open Specs
 open LiveData
 open Feeds
 open Utils
+open Microsoft.Azure.Devices.Client
+open System.Text
+open System.Configuration
+open System.Threading
 
 let gev s = Environment.GetEnvironmentVariable s
 
 let baseUrl = gev "SUNSHINE_URL"
 let username = gev "SUNSHINE_USERNAME"
 let password = gev "SUNSHINE_PASSWORD"
+let iotHubConnectionString = gev "IOT_CONNSTR"
 
 [<EntryPoint>]
 let main _ =
-   async {
+    let iotHubClient = DeviceClient.CreateFromConnectionString(iotHubConnectionString, TransportType.Mqtt)
+
+    async {
             // do! System.Threading.Tasks.Task.Delay(5000) |> Async.AwaitTask
             let token = getAuthToken username password
             let getData' = getData token (Uri baseUrl)
@@ -23,23 +28,51 @@ let main _ =
             let! specs = getSpec getData'
             let deviceId = specs.Device.DeviceId |> toS
 
+            printfn "Up and running, we found an inverter with the ID %s" deviceId
+
             match! getLiveList getData' deviceId with
             | Some liveList ->
                 printfn "Found the live list"
+                printfn "%A" liveList
+
+                do! sendIoTMessage iotHubClient liveList
+
+                let rec dataPoller() = async {
+                    match! getLiveData getData' deviceId with
+                    | Some liveData ->
+                        printfn "Found the live data"
+                        printfn "%A" liveData
+
+                        do! sendIoTMessage iotHubClient liveData
+
+                    | None ->
+                        printfn "Didn't find live data"
+
+                    do! Async.Sleep 20000
+                    dataPoller() |> Async.Start
+                }
+
+                dataPoller() |> Async.Start
+
             | None ->
                 printfn "Well that shouldn't have happened..."
 
-            match! getPgridFeed getData' DateTime.Today deviceId with
-            | Some feed ->
-                printfn "Found the data feed"
-            | None ->
-                printfn "Didn't find feed"
+            let rec feedPoller() = async {
+                match! getPgridFeed getData' DateTime.Today deviceId with
+                | Some feed ->
+                    printfn "Found the data feed"
+                    printfn "%A" feed
+                | None ->
+                    printfn "Didn't find feed"
 
-            match! getLiveData getData' deviceId with
-            | Some liveData ->
-                printfn "Found the live data"
-            | None ->
-                printfn "Didn't find live data"
+                do! Async.Sleep 300000
+                feedPoller() |> Async.Start
+            }
+
+            feedPoller() |> Async.Start
+
+            printfn "Background jobs running, now we're waiting... "
+            Console.ReadLine() |> ignore
 
             return 0
     } |> Async.RunSynchronously
--- src/Sunshine.Downloader/Utils.fs
+++ src/Sunshine.Downloader/Utils.fs
@@ -1,3 +1,11 @@
 module Utils
+open Microsoft.Azure.Devices.Client
+open System.Text
+open System
 
 let inline toS o = o.ToString()
+
+let sendIoTMessage (client : DeviceClient) (obj : Object) =
+    let json = obj |> toS
+    let msg = new Message(Encoding.ASCII.GetBytes json)
+    client.SendEventAsync(msg) |> Async.AwaitTask
