5ac4100dda9569deceaf6f88aa3ecde54df51e50
Aaron Powell <me@aaron-powell.com>
2019-04-29 14:28:14 +1000

    pinning versions of the Azure Storage API to hope F# doesn't break

--- src/Sunshine.Functions/LiveDataTrigger.fs
+++ src/Sunshine.Functions/LiveDataTrigger.fs
@@ -4,10 +4,23 @@ open Microsoft.Extensions.Logging
 open Microsoft.Azure.WebJobs
 
 open LiveData
+open Microsoft.WindowsAzure.Storage.Table
+
+type LiveDataEntity(partitionKey, rowKey) =
+        inherit TableEntity(partitionKey, rowKey)
 
 [<FunctionName("LiveDataTrigger")>]
 let trigger
     ([<EventHubTrigger("live-data", Connection = "IoTHubConnectionString")>] message: string)
+    ([<Table("LiveData", Connection = "DataStorageConnectionString")>] liveDataTable: CloudTable)
     (logger: ILogger) =
         let l = LiveData.Parse message
+
+        let id = l.DeviceId.DeviceId.ToString()
+
+        // let op = TableOperation.Insert(LiveDataEntity("", ""))
+
+        // liveDataTable.ExecuteAsync op |> Async.AwaitTask |> Async.RunSynchronously |> ignore
+
         logger.LogInformation(sprintf "Live Data: %A" l)
+        logger.LogInformation(sprintf "Id: %s" id)
