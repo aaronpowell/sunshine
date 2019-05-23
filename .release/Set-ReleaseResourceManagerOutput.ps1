f02b909f8a83de2978798dff292e9b850457810c
Aaron Powell <me@aaron-powell.com>
2019-05-23 16:21:55 +1000

    Trying to use pipeline artifacts

--- .release/Set-ReleaseResourceManagerOutput.ps1
+++ .release/Set-ReleaseResourceManagerOutput.ps1
@@ -1,17 +1,7 @@
 param (
     [Parameter(Mandatory=$true)]
     [string]
-    $ResourceManagerOutput,
-
-    [Parameter(Mandatory=$true)]
-    [string]
-    $DeploymentTemplatePath
+    $ResourceManagerOutput
     )
 
-$rmJson = ConvertFrom-Json $ResourceManagerOutput
-
-Write-Host "##vso[task.setvariable variable=FunctionAppName;isOutput=true]$($rmJson.functionAppName.value)"
-Write-Host "##vso[task.setvariable variable=IoTHubName;isOutput=true]$($rmJson.iotHubName.value)"
-
-$deploymentJson = Get-Content $DeploymentTemplatePath | ConvertFrom-Json
-Write-Host "##vso[task.setvariable variable=DEVOPS_IOTEDGE_REGISTRY_URL;isOutput=true]$($deploymentJson.modulesContent.'$edgeAgent'.'properties.desired'.runtime.settings.registryCredentials.YourACR.address.value)"
+Set-Content -Path $(env:SYSTEM_DEFAULTWORKINGDIRECTORY)/.arm/release-output.json -Value $ResourceManagerOutput
