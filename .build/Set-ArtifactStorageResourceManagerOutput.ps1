3325423b68d7bcee22ff353d68d1d354454c023e
Aaron Powell <me@aaron-powell.com>
2019-05-24 09:18:40 +1000

    Pushing some artifacts to storage to make them private

---
+++ .build/Set-ArtifactStorageResourceManagerOutput.ps1
@@ -0,0 +1,9 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ResourceManagerOutput
+    )
+
+$json = ConvertFrom-Json $ResourceManagerOutput
+
+Write-Host "##vso[task.setvariable variable=ARTIFACT_STORAGE_NAME;isOutput=true]$($json.artifactStorageName.value)"
