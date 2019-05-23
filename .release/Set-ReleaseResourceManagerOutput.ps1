eff3d398c4fa2090109ae20b68b8233bad209957
Aaron Powell <me@aaron-powell.com>
2019-05-23 15:51:46 +1000

    why was I passing the ACR name as the IOT hub name? 🤦‍♂️

--- .release/Set-ReleaseResourceManagerOutput.ps1
+++ .release/Set-ReleaseResourceManagerOutput.ps1
@@ -11,7 +11,7 @@ param (
 $rmJson = ConvertFrom-Json $ResourceManagerOutput
 
 Write-Host "##vso[task.setvariable variable=FunctionAppName;isOutput=true]$($rmJson.functionAppName.value)"
-Write-Host "##vso[task.setvariable variable=IoTHubName;isOutput=true]$($rmJson.IoTHubName.value)"
+Write-Host "##vso[task.setvariable variable=IoTHubName;isOutput=true]$($rmJson.iotHubName.value)"
 
 $deploymentJson = Get-Content $DeploymentTemplatePath | ConvertFrom-Json
 Write-Host "##vso[task.setvariable variable=DEVOPS_IOTEDGE_REGISTRY_URL;isOutput=true]$($deploymentJson.modulesContent.'$edgeAgent'.'properties.desired'.runtime.settings.registryCredentials.YourACR.address.value)"
