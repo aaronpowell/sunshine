62dc68608c9f0eed1ea6a3f12b4bb39ee55e66e9
Aaron Powell <me@aaron-powell.com>
2019-05-23 13:16:52 +1000

    using the variable correctly

--- .release/Set-ReleaseResourceManagerOutput.ps1
+++ .release/Set-ReleaseResourceManagerOutput.ps1
@@ -9,5 +9,5 @@ $rmJson = ConvertFrom-Json $ResourceManagerOutput
 Write-Host "##vso[task.setvariable variable=FunctionAppName;isOutput=true]$($rmJson.functionAppName.value)"
 Write-Host "##vso[task.setvariable variable=IoTHubName;isOutput=true]$($rmJson.IoTHubName.value)"
 
-$deploymentJson = Get-Content $(System.DefaultWorkingDirectory)/_aaronpowell.sunshine/drop/deployment.arm32v7.json | ConvertFrom-Json
+$deploymentJson = Get-Content $(env:SYSEM_DEFAULTWORKINGDIRECTORY)/_aaronpowell.sunshine/drop/deployment.arm32v7.json | ConvertFrom-Json
 Write-Host "##vso[task.setvariable variable=DEVOPS_IOTEDGE_REGISTRY_URL;isOutput=true]$($deploymentJson.modulesContent.'$edgeAgent'.'properties.desired'.runtime.settings.registryCredentials.YourACR.address.value)"
