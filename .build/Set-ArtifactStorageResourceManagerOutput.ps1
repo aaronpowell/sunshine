8700b301235a216e6472959a667c8afcfd266a14
Aaron Powell <me@aaron-powell.com>
2019-05-24 12:17:02 +1000

    Adding a debugging line

--- .build/Set-ArtifactStorageResourceManagerOutput.ps1
+++ .build/Set-ArtifactStorageResourceManagerOutput.ps1
@@ -7,3 +7,5 @@ param (
 $json = ConvertFrom-Json $ResourceManagerOutput
 
 Write-Host "##vso[task.setvariable variable=ARTIFACT_STORAGE_NAME;isOutput=true]$($json.artifactStorageName.value)"
+
+Write-Host $json.artifactStorageName.value
