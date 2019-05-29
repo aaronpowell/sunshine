dc70b3495ee5ac631130ed0c9b28d187d24f4278
Aaron Powell <me@aaron-powell.com>
2019-05-29 16:12:35 +1000

    removing trailing spaces

--- .release/set-module-twin.sh
+++ .release/set-module-twin.sh
@@ -4,8 +4,8 @@ az extension add --name azure-cli-iot-ex
 
 IoTHubName=$(cat $SYSTEM_DEFAULTWORKINGDIRECTORY/_aaronpowell.sunshine/arm/release-output.json | jq '.iotHubName.value' | sed s/\"//g)
 
-az iot hub module-twin update \ 
---device-id $DEVICE_ID \ 
---hub-name $IoTHubName \ 
---module-id SunshineDownloader \ 
+az iot hub module-twin update \
+--device-id $DEVICE_ID \
+--hub-name $IoTHubName \
+--module-id SunshineDownloader \
 --set properties.desired='{ "inverter": { "username": "$SunshineUser", "password": "$SunshinePwd", "url": "$SunshineUrl" } }'
